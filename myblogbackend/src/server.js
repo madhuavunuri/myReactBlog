e176902fe657ced8c4138af77acbe8284ce33654
Madhu Avunuri <madhu.avunuri524@gmail.com>
2022-12-29 17:10:42 +0100

    Create UpVote API for articles
    * upvoting is basically likes for the articles we have in DB

--- myblogbackend/src/server.js
+++ myblogbackend/src/server.js
@@ -1,60 +1,59 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 
-
 const app = express();
 app.use(express.json());
 
-app.get("/api/articles/:name", async (req, res)=>
-{
-  const {name } = req.params;
+app.get("/api/articles/:name", async (req, res) => {
+  const { name } = req.params;
+
   const client = new MongoClient("mongodb://127.0.0.1:27017");
   await client.connect();
 
   const db = client.db("react-blog-db");
+  const article = await db.collection("articles").findOne({ name });
 
-  const article = await db.collection("articles").findOne({name});
-  if(article)
-  {
+  if (article) {
     res.json(article);
-  }
-  else
-  {
+  } else {
     res.sendStatus(404);
   }
-  
 });
 
-app.put("/api/articles/:name/upvote", (req, res) => {
+app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
 
-  const article = articlesInfo.find(art => art.name === name);
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+
+  const db = client.db("react-blog-db");
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $inc: { upvotes: 1 },
+    }
+  );
+
+  const article = await db.collection("articles").findOne({ name });
   if (article) {
-    article.upvotes += 1;
     res.send(`Article ${article.name} up voted - total :${article.upvotes}`);
-  }
-  else
-  {
+  } else {
     res.send(`Article not found`);
   }
 });
 
-app.post("/api/articles/:name/comments", (req, res) =>
-{
-    const {name} = req.params;
-    const { postedBy , text} = req.body;
-    console.log(req.body);
-    console.log({ postedBy , text});
-    const article = articlesInfo.find(art => art.name === name);
-    if(article)
-    {
-        article.comments.push({postedBy , text});
-        res.send(article.comments);
-    }
-    else
-    {
-        res.send(`Article not found`);
-    }
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+  console.log(req.body);
+  console.log({ postedBy, text });
+  const article = articlesInfo.find((art) => art.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send(`Article not found`);
+  }
 });
 
 app.listen(8000, () => {
