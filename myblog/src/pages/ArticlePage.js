61729c54f8ebf6c6a8b6459bb29bb7d8701f251d
Madhu Avunuri <madhu.avunuri524@gmail.com>
2022-12-30 20:45:00 +0100

    Add Axios Package

--- myblog/src/pages/ArticlePage.js
+++ myblog/src/pages/ArticlePage.js
@@ -1,9 +1,25 @@
+import axios from "axios";
+import { useState , useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
   const { articleId } = useParams();
+
+  const [articleInfo , setArticleInfo] = useState({upvotes : 0 , comments:[]});
+
+  useEffect(() => {
+
+    const loadArticleInfo = async () =>
+    {
+      const response = await axios.get(`/api/articles/${articleId}`);
+
+      setArticleInfo(response.data);
+    }
+    loadArticleInfo();
+  },[]);
+  
   const article = articles.find((ar) => ar.name === articleId);
 
   if(!article) return <NotFoundPage/>
@@ -11,6 +27,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvotes</p>
       {article.content.map((par, i) => (
         <p key={i}>{par}</p>
       ))}
