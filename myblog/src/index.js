9d1f5ff99eed8c2cd1b5ac55646aca06147902c9
Madhu Avunuri <madhu.avunuri524@gmail.com>
2022-12-30 23:16:44 +0100

    Add Firebase Package

--- myblog/src/index.js
+++ myblog/src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4w6u3AWRq0zcMUL1qxKrK1T2fNdD4VSU",
+  authDomain: "my-react-blog-9c5a4.firebaseapp.com",
+  projectId: "my-react-blog-9c5a4",
+  storageBucket: "my-react-blog-9c5a4.appspot.com",
+  messagingSenderId: "647499240230",
+  appId: "1:647499240230:web:2f28e872d5e4caf303e7e7",
+  measurementId: "G-KTXFM3QJJL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
